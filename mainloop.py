01b563eacb4e11f77b436ab86122ae3a1979dd62
billthefighter <billthefighter@gmail.com>
2015-09-19 21:09:03 -0700

    Added sparkletest to make sparkles
    First attempt to make class “Star”. Commented region in class “star” is
    my first attempt where you passed values; was more elegant to
    initialize as random from the beginning to reduce calls.

--- mainloop.py
+++ mainloop.py
@@ -1,13 +1,35 @@
 #Alarm clock thing
-
-#init variables
-
-#init classes
 import time
+import Image
+import ImageDraw
 from rgbmatrix import Adafruit_RGBmatrix
-
+#init variables
+width = 32
+height = 32
+image = Image.new("1", (width, height)) # Can be larger than matrix if wanted!!
+draw  = ImageDraw.Draw(image)    # Declare Draw instance before prims
 # Rows and chain length are both required parameters:
 matrix = Adafruit_RGBmatrix(32, 1)
+image       = Image.new('L', (width, height))
+draw        = ImageDraw.Draw(image)
+currentTime = 0.0
+prevTime    = 0.0
+#init classes
+class star:
+	def __init__(self, x, y, sp):
+		self.x = x
+		self.y = y
+		self.sp = sp #sparkle value
+
+	def sparkle(self):
+		if self.sp == 0:
+			self.sp += 1
+			pass
+		if self.sp > 0 and self.sp < 8:
+			self.sp += 1
+			pass
+
+
 
 # Flash screen red, green, blue (packed color values)
 matrix.Fill(0xFF0000)
@@ -16,6 +38,10 @@ matrix.Fill(0x00FF00)
 time.sleep(1.0)
 matrix.Fill(0x0000FF)
 time.sleep(1.0)
+matrix.Clear()
+
+draw.point(xy, options)
+draw.ellipse(xy, options)
 
 #init current time
 
@@ -33,6 +59,36 @@ time.sleep(1.0)
 #state 2: Nighttime star sparkle mode
 	#ends with timeout
 	#ends with button, back to state 0
+
+	# Initialization done; loop forever ------------------------------------------
+while True:
+
+	# Clear background
+	draw.rectangle((0, 0, width, height), fill=(0, 0, 0))
+
+	for t in tileList:
+		if t.x < width:        # Draw tile if onscreen
+			t.draw()
+		t.x -= 1               # Move left 1 pixel
+		if(t.x <= -tileWidth): # Off left edge?
+			t.x += tileWidth * tilesAcross     # Move off right &
+			t.p  = predictList[nextPrediction] # assign prediction
+			nextPrediction += 1                # Cycle predictions
+			if nextPrediction >= len(predictList):
+				nextPrediction = 0
+
+	# Try to keep timing uniform-ish; rather than sleeping a fixed time,
+	# interval since last frame is calculated, the gap time between this
+	# and desired frames/sec determines sleep time...occasionally if busy
+	# (e.g. polling server) there'll be no sleep at all.
+	currentTime = time.time()
+	timeDelta   = (1.0 / fps) - (currentTime - prevTime)
+	if(timeDelta > 0.0):
+		time.sleep(timeDelta)
+	prevTime = currentTime
+
+	# Offscreen buffer is copied to screen
+	matrix.SetImage(image.im.id, 0, 0)
 #state 3: Sexytime
 	#ends with timeout
 	#ends with button, back to state 0
@@ -41,4 +97,6 @@ time.sleep(1.0)
 	#exits on button press to stocktick
 	#exits on timeout
 #state5: Stocktick
+#State6: Time Display
+#state7: Reading light 
 	#
---
+++ sparkletest.py
@@ -0,0 +1,60 @@
+# test to debug sparkle class in pure python before moving to matrix
+import random
+
+class star:
+	# def __init__(self, x, y, sp, toggle):
+	# 	self.x = x
+	# 	self.y = y
+	# 	self.sp = sp #sparkle value
+	# 	self.toggle = toggle
+	def __init__(self):
+		self.x = random.randint(0, 32)
+		self.y = random.randint(0, 32)
+		self.sp = random.randint(0,8)
+		self.toggle = random.randint(0,1)
+
+	def sparkle(self):
+		if self.sp >= 0 and self.sp < 8 and self.toggle == 0:
+			self.sp += 1
+			pass
+		elif self.sp == 8:
+			self.toggle == 1
+			self.sp -= 1
+		elif self.toggle == 1 and self.sp > 0:
+			self.sp -= 1
+	def joggle():
+		self.x = random.randint(0, 32)
+		self.y = random.randint(0, 32)
+		self.sp = random.randint(0,8)
+		self.toggle = random.randint(0,1)
+
+
+
+		
+		
+x = star()
+print x.sp
+
+#generate initial list of stars
+# starlist = [] 
+# for i in xrange(0,10):
+# 	starlist.append(star(makestar()))
+# 	print starlist[i].x,starlist[i].y,starlist[i].sp,starlist[i].toggle
+# 	pass
+
+
+# #loop through stars
+# for i in xrange(starlist):
+#  	if starlist[i].sp = 0 and fartarray[i].toggle = 1: #if it's in the list and has completed sparkling, this condition will be true
+#  			fartarray.pop(i)
+#  			pass
+#  	if fartarray[i].sp != 0 :
+#  			fartarray.sparkle()
+#  			pass	
+#  	pass
+# #check how many stars there are in the list, if there's less than 10 then make a new star
+# if len(starlist) < 10:
+
+
+
+#
