8b3ae6c2d7f75d9d71c7480ef60ff57e895be465
billthefighter <billthefighter@gmail.com>
2015-09-19 23:13:18 -0700

    Updated code in maintop
    Updated main loop code, also need to finish implementing newstar

--- sparkletest.py
+++ sparkletest.py
@@ -1,5 +1,10 @@
 # test to debug sparkle class in pure python before moving to matrix
 import random
+import time
+
+#global inits
+fps            = 2  # Scrolling speed (ish)
+prevTime    = 0.0
 
 class star:
 	def __init__(self):
@@ -17,11 +22,11 @@ class star:
 			self.sp -= 1
 		elif self.toggle == 1 and self.sp > 0:
 			self.sp -= 1
-	def joggle():
+	def newstar():
 		self.x = random.randint(0, 32)
 		self.y = random.randint(0, 32)
-		self.sp = random.randint(0,8)
-		self.toggle = random.randint(0,1)
+		self.sp = 0
+		self.toggle = 0
 
 
 
@@ -32,33 +37,35 @@ for i in xrange(0,10):
 	#print starlist[i].x,starlist[i].y,starlist[i].sp,starlist[i].toggle
 	pass
 
-poop =0
-x = star()
+poop = 0
 while poop < 10:
-	print poop
-	x.sparkle()
-	print x.x,x.y,x.sp,x.toggle
-	# #print poop
-	# #print 'starting starloop'
-	# #loop through stars
-	# for i in xrange(len(starlist)):
-	#  	if starlist[i].sp == 0 and starlist[i].toggle == 1: #if it's in the list and has completed sparkling, this condition will be true
-	#  			starlist.pop(i)
-	#  			starlist.append(star()) #since we might have popped a star off the list, the loop can fail if the list suddenly becomes smaller
-	#  			pass
-	#  	else:
-	#  			starlist[i].sparkle()
-	#  			pass	
-	#  	pass
-	# #check how many stars there are in the list, if there's less than 10 then make a new star
-	# if len(starlist) < 10:
-	# 	starlist.append(star())
-	# poop += 1
-	# print starlist[0].x,starlist[0].y,starlist[0].sp,starlist[0].toggle
-	# #for i in xrange(1,10):
-	# 	#print starlist[i].x,starlist[i].y,starlist[i].sp,starlist[i].toggle
-	# 	#pass
+	#print poop
+	#print 'starting starloop'
+	#loop through stars
+	for i in xrange(len(starlist)):
+	 	if starlist[i].sp == 0 and starlist[i].toggle == 1: #if it's in the list and has completed sparkling, this condition will be true
+	 			starlist.pop(i)
+	 			starlist.append(star()) #since we might have popped a star off the list, the loop can fail if the list suddenly becomes smaller
+	 			#NOTE NOTE NOTE Need to make sure to newstar the newly appended star or it will just pop on at some random value
+	 			pass
+	 	else:
+	 			starlist[i].sparkle()
+	 			pass	
+	 	pass
+	#check how many stars there are in the list, if there's less than 10 then make a new star
+	if len(starlist) < 10:
+		starlist.append(star())
+		#NOTE NOTE NOTE Need to make sure to newstar the newly appended star or it will just pop on at some random value
+	print starlist[0].x,starlist[0].y,starlist[0].sp,starlist[0].toggle
+	#for i in xrange(1,10):
+	#	print starlist[i].x,starlist[i].y,starlist[i].sp,starlist[i].toggle
+	#	pass
 	poop += 1	
+	currentTime = time.time()
+	timeDelta   = (1.0 / fps) - (currentTime - prevTime)
+	if(timeDelta > 0.0):
+		time.sleep(timeDelta)
+	prevTime = currentTime
 	pass
 
 
