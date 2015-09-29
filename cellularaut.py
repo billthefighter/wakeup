6baddd94a68d1e921ecf6c64650c8b37c23a04d7
billthefighter <billthefighter@gmail.com>
2015-09-28 23:06:49 -0700

    trying to modify cellular aut to run on panel

--- cellularaut.py
+++ cellularaut.py
@@ -1,7 +1,8 @@
 from PIL import Image
 from PIL import ImageOps
 import random
-
+#from rgbmatrix import Adafruit_RGBmatrix
+import atexit
 #define inputs
 #runrule = 3
 
@@ -30,19 +31,21 @@ def basicRun(rule, steps, stepper, seed=[1], k=2, r=1):
     return result, (len(seed), steps + 1)
 
 def showResult(result, dims, k=2):
-    i = Image.new("L", dims)
+    i = Image.new("RGB", dims)
     i.putdata(result, (255 / (k - 1)))
     i = i.crop(i.getbbox())
     i = ImageOps.invert(i)
     i.load()
     i.show()
+    matrix.SetImage(image.im.id, 0, 0)
 
 def runTest(runrule,lines):
     result, dims = basicRun(runrule, lines, step)
     showResult(result, dims)
 
 if __name__ == "__main__":
-    for x in [30,90,54,110]:
-        runTest(x,32)
-        pass
+    runTest(90,32)
+    #for x in [30,90,54,110]:
+        #runTest(x,32)
+        #pass
     
